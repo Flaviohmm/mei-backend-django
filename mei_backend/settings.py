b7b45bf42bb97bf40f7375cdb13d8f4d5f96b339
Flavio Macedo <fhenrique609@gmail.com>
2025-06-14 16:57:21 -0300

    Configurando o email backend para o gmail

--- mei_backend/settings.py
+++ mei_backend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-43)%@me+4tl6p6*7&*n35es=ap93)0+wskypd9*l5+9*6=yqvj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,13 +78,13 @@ CORS_ALLOWED_ORIGINS = [
 
 # Configurações de Email
 # Para produção com Gmail (descomente e configure)
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'seu-email@gmail.com'
-# EMAIL_HOST_PASSWORD = 'sua-senha-de-app'  # Use App Password, não senha normal
-# DEFAULT_FROM_EMAIL = 'seu-email@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # Usando a variável de ambiente
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Usando a variável de ambiente
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 # Para outros provedores (exemplo: SendGrid)
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -94,7 +99,7 @@ CORS_ALLOWED_ORIGINS = [
 FRONTEND_URL = 'http://localhost:8080'
 
 # Para desenvolvimento, você pode usar o console backend:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Para debug
 LOGGING = {
